59ef34ab47c2d6b87357998eb107f789e15622d7
Nora <nsansaturnino@birt.eus>
2021-11-21 08:50:34 +0100

    PMDM02 Tarea 1: Propuesta de solución

--- src/app/services/gestion-noticias-leer.service.ts
+++ src/app/services/gestion-noticias-leer.service.ts
@@ -13,12 +13,27 @@ export class GestionNoticiasLeerService {
   }
 
   addNoticia(item) {
+    // copiar item
+    let itemString = JSON.stringify(item);
+    item = JSON.parse(itemString);
+
+    // Añadirlo
     this.noticiasLeer.push(item);
     // console.log(this.noticiasLeer);
   }
 
+  buscar(item: Article): number  {
+    let articuloEncontrado: Article = this.noticiasLeer.find(
+      function(cadaArticulo) { 
+        return JSON.stringify(cadaArticulo) == JSON.stringify(item);
+      }
+    );
+    let indice = this.noticiasLeer.indexOf(articuloEncontrado);
+    return indice;
+  }
+
   borrarNoticia(item) {
-    let indice = this.noticiasLeer.indexOf(item);
+    let indice = this.buscar(item);
     if (indice != -1) {
       this.noticiasLeer.splice(indice, 1);
       // console.log(this.noticiasLeer); 
